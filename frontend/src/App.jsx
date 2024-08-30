b10a2b1ec27dbb45ec668616f49d206b30cdb2a8
Ugochukwu Augustine <austinjasper07@gmail.com>
2024-08-30 19:05:04 +0100

    Created the Pages and Routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,24 @@
-import React from 'react'
+import {Routes, Route} from "react-router-dom"
+import Header from "./components/Header"
+import Footer from "./components/Footer"
+import HomePage from "./pages/HomePage"
+import SignUp from "./pages/SignUp"
+import Login from "./pages/Login"
+import About from "./pages/About"
+import NotFoundPage from "./pages/NotFoundPage"
 
 export default function App() {
   return (
-    <div>App</div>
+    <>
+      <Header />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/sign_up" element={<SignUp />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/about" element={<About />} />
+        <Route path="*" element={<NotFoundPage />} />
+      </Routes>
+      <Footer/>
+    </>
   )
 }
